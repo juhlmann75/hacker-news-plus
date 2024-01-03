9ff230ebce8d63c39fe5cf1f6c947fec6e1d3d4d
juhlmann75 <juhlmann75@gmail.com>
2024-01-03 16:59:42 -0600

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,17 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
 import {ThemeProvider} from "next-themes";
+import {Analytics} from '@vercel/analytics/react';
 
-function HackerNewsPlus({ Component, pageProps }: AppProps) {
-  return (
-      <>
-        <ThemeProvider attribute="class">
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </>
-  )
+function HackerNewsPlus({Component, pageProps}: AppProps) {
+    return (
+        <>
+            <ThemeProvider attribute="class">
+                <Component {...pageProps} />
+                <Analytics/>
+            </ThemeProvider>
+        </>
+    )
 }
 
 export default HackerNewsPlus
